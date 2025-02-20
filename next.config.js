e03ffd3a3ac3d70c900fac4a6da8f8279f0eb9fb
Yura <linetsky.yura@gmail.com>
2025-02-20 18:57:02 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    distDir: process.env.NODE_ENV === 'production' ? '.next-prod' : '.next',
     typescript: {
         ignoreBuildErrors: true,
     },
